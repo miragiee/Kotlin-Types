1456b8c2b4528ecc2f553474c398fa25d0fd7a6e
miragiee <miragiee564@gmail.com>
2026-09-09 16:28:22 +0300

    Делаю задание 3

--- src/Task03.kt
+++ src/Task03.kt
@@ -1,12 +1,18 @@
-fun getCoordinatesFromString(coordinates : string) {
+import kotlin.math.sqrt
+
+fun getCoordinatesFromString(coordinates : string) : Pair<Float, Float> {
     var x : float = point1.substringBefore(',').toFloat()
     var y : float = point1.substringAfter(',').trimStart().toFloat()
+
+    return Pair(x, y)
 }
 
 fun main() {
     print("Введите координаты первой точки в формате X Y")
-
-    var point1 : string = readln()
+    var point1 : string = getCoordinatesFromString(readln())
 
     print("\n Введите координаты второй точки")
+    var point2 : string = getCoordinatesFromString(readln());
+
+    var result = sqrt()
 }
